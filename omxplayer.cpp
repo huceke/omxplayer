65c1192941febe5457a6401267ddf2512f843674
gimli <gimli@xbmc.org>
2012-06-11 07:23:04 +0200

    use + for volume up. so volume controll is -/+

--- omxplayer.cpp
+++ omxplayer.cpp
@@ -559,7 +559,7 @@ int main(int argc, char *argv[])
         m_player_audio.SetCurrentVolume(m_player_audio.GetCurrentVolume() - 50);
         printf("Current Volume: %.2fdB\n", m_player_audio.GetCurrentVolume() / 100.0f);
         break;
-      case '=':
+      case '+':
         m_player_audio.SetCurrentVolume(m_player_audio.GetCurrentVolume() + 50);
         printf("Current Volume: %.2fdB\n", m_player_audio.GetCurrentVolume() / 100.0f);
         break;
