9b0793faf2c12f49b743ae3778ece03e31ed1538
popcornmix <popcornmix@gmail.com>
2014-03-08 13:54:19 +0000

    Enable https and openssl. Bump to 2.1.4

--- OMXReader.cpp
+++ OMXReader.cpp
@@ -132,7 +132,7 @@ bool OMXReader::Open(std::string filename, bool dump_format, bool live /* =false
     m_filename.replace(0, 8, "http://");
 
   if(m_filename.substr(0,6) == "mms://" || m_filename.substr(0,7) == "mmsh://" || m_filename.substr(0,7) == "mmst://" || m_filename.substr(0,7) == "mmsu://" ||
-      m_filename.substr(0,7) == "http://" || 
+      m_filename.substr(0,7) == "http://" || m_filename.substr(0,8) == "https://" ||
       m_filename.substr(0,7) == "rtmp://" || m_filename.substr(0,6) == "udp://" ||
       m_filename.substr(0,7) == "rtsp://" )
   {
