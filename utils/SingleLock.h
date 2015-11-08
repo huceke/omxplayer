a2fcde541108d667699d5378bb97d015f9c1e1a0
Oleksandr Tymoshenko <gonzo@bluezbox.com>
2015-11-08 03:44:07 +0000

    Do not create copy of CCriticalSection for each CSingleLock instance
    Use reference to CCriticalSection as a CSingleLock member. Otherwise
    assignment operator just byte-copies content of CCriticalSection
    and for each copy destructor will destroy mutex which may lead
    to undefined behaviour

--- utils/SingleLock.h
+++ utils/SingleLock.h
@@ -40,6 +40,10 @@ public:
   inline void Lock()         { pthread_mutex_lock(&m_lock); }
   inline void Unlock()       { pthread_mutex_unlock(&m_lock); }
 
+private:
+  CCriticalSection(CCriticalSection &other) = delete;
+  CCriticalSection& operator=(const CCriticalSection&) = delete;
+
 protected:
   pthread_mutex_t m_lock;
 };
@@ -48,11 +52,11 @@ protected:
 class CSingleLock
 {
 public:
-  inline CSingleLock(CCriticalSection& cs) { m_section = cs; m_section.Lock(); }
+  inline CSingleLock(CCriticalSection& cs) : m_section(cs) { m_section.Lock(); }
   inline ~CSingleLock()                    { m_section.Unlock(); }
 
 protected:
-  CCriticalSection m_section;
+  CCriticalSection &m_section;
 };
 
 
