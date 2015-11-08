0415cda287835b4ae231114e35758e6139bb7d23
Oleksandr Tymoshenko <gonzo@bluezbox.com>
2015-11-08 03:41:29 +0000

    Fix order of operations on pthread attribute: init first, then modify

--- OMXThread.cpp
+++ OMXThread.cpp
@@ -41,8 +41,8 @@
 OMXThread::OMXThread()
 {
   pthread_mutex_init(&m_lock, NULL);
-  pthread_attr_setdetachstate(&m_tattr, PTHREAD_CREATE_JOINABLE);
   pthread_attr_init(&m_tattr);
+  pthread_attr_setdetachstate(&m_tattr, PTHREAD_CREATE_JOINABLE);
   m_thread    = 0;
   m_bStop     = false;
   m_running   = false;
