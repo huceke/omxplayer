ceda43a0d0ddb06786fa287f432fbd47650f457c
Laurent Marchelli <laurent.marchelli@gmail.com>
2016-04-28 23:58:40 +0200

    dbuscontrol.sh bash file fixed (#443)

--- dbuscontrol.sh
+++ dbuscontrol.sh
@@ -2,8 +2,8 @@
 
 #set -x
 
-OMXPLAYER_DBUS_ADDR="/tmp/omxplayerdbus.${USER}"
-OMXPLAYER_DBUS_PID="/tmp/omxplayerdbus.${USER}.pid"
+OMXPLAYER_DBUS_ADDR="/tmp/omxplayerdbus.${USER:-root}"
+OMXPLAYER_DBUS_PID="/tmp/omxplayerdbus.${USER:-root}.pid"
 export DBUS_SESSION_BUS_ADDRESS=`cat $OMXPLAYER_DBUS_ADDR`
 export DBUS_SESSION_BUS_PID=`cat $OMXPLAYER_DBUS_PID`
 
